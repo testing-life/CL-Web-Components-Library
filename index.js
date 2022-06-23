5f4e0a1c92e0957c7dd9a5d99a9c507cb00f0daf
Mariusz Hussariush <mariusz@curvelabs.eu>
2022-06-23 13:18:38 +0200

    Import fixes

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-import './components/MaxInput/index.js';
-import './components/TrimAddress/index.js';
+export * as MaxInput from './components/MaxInput/index.js';
+export * as TrimAddress from './components/TrimAddress/index.js';
+export * as Services from './services/index.js';
